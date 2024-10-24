8cb3cf1050f6f50ca14fa86dae39f85fb4116dd4
bokyum <95bogyeom@gmail.com>
2024-10-25 00:42:34 +0900

    navigation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,11 @@
+import Navigation from "../components/navigation";
+
 export default function NotFound() {
-  return <h1>NOT FOUND!!</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>NOT FOUND!!</h1>
+    </div>
+  );
+  // return <h1>NOT FOUND!!</h1>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Navigation from "../components/navigation";
+
 export default function Page() {
-  return <h1>Hello NextJ!</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Hello NextJ!</h1>
+    </div>
+  );
 }
