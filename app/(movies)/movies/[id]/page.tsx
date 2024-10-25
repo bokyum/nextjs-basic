573c297c808223114bf8bd0633cab67f7b88a038
bokyum <95bogyeom@gmail.com>
2024-10-25 17:10:02 +0900

    Parallel Request(Promise.all)

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,4 +1,5 @@
 import React, { use } from "react";
+import { API_URL } from "../../../(home)/page";
 
 /**
  {
@@ -6,11 +7,30 @@ import React, { use } from "react";
     id: '12334',
 } 
  */
-export default function MovieDetail({
+
+async function getMovie(id: string) {
+  console.log(`Fetching Movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+async function getVideos(id: string) {
+  console.log(`Fetching Videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  //   const movie = await getMovie(id);
+  //   const videos = await getVideos(id);
+  console.log("end fetching");
   return <h1>Movie {id}</h1>;
 }
 
