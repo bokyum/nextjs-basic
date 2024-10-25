4c22f1864f458420c85e10b26919618826cdcb7b
bokyum <95bogyeom@gmail.com>
2024-10-25 16:04:24 +0900

    NextJs Server Side Fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,16 @@
-"use client";
 import React, { use, useEffect } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [movies, setMovies] = React.useState([]);
+export const metadata = {
+  title: "Home",
+};
+const getMovies = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log("fetching movies");
+  return fetch(URL).then((response) => response.json());
+};
 
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const data = await response.json();
-    setMovies(data);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
