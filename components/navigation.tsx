82f0d1a6308837b253b9af51279423018bf64e57
bokyum <95bogyeom@gmail.com>
2024-10-25 14:00:40 +0900

    use clinet ~= hydrate, dehydrate

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
   console.log("path", path);
   return (
     <nav>
@@ -14,6 +16,11 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "📝" : ""}
+        </li>
+        <li>
+          <button onClick={() => setCount((count) => count + 1)}>
+            Click me {count}
+          </button>
         </li>
       </ul>
     </nav>
